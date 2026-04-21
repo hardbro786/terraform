0ff2afeb09cb3d8614ff975cc5bef27cfc9fd715
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-21 15:49:46 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "instance_id" {
-  value = aws_instance.private_instance.id
+  description = "EC2 Instance ID"
+  value       = aws_instance.private_instance.id
 }
-
+ 
 output "private_ip" {
-  value = aws_instance.private_instance.private_ip
+  description = "Private IP of EC2 Instance"
+  value       = aws_instance.private_instance.private_ip
 }
-
+ 
 output "ssh_note" {
-  value = "Use Bastion or SSM to connect (No Public IP)"
+  description = "Reminder: use Bastion or SSM to connect"
+  value       = "Use Bastion or SSM to connect — No Public IP assigned"
 }
