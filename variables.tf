037a284338119aaa3c2edd925e7b185942fbecbd
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-21 15:49:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,35 @@
 variable "region" {
-  default = "us-east-1"
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
 }
-
+ 
 variable "ami_id" {
-  description = "AMI ID for EC2"
+  description = "AMI ID for EC2 instance"
+  type        = string
   default     = "ami-033bd18826652770f"
 }
-
+ 
 variable "instance_type" {
-  default = "t3.small"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.small"
 }
-
+ 
 variable "key_name" {
-  description = "SSH Key Name"
-  default     = "my-terraform-key"
+  description = "SSH Key Pair Name"
+  type        = string
+  default     = "otms-dev-key"
 }
-
+ 
 variable "project" {
-  default = "otms"
+  description = "Project name"
+  type        = string
+  default     = "otms"
 }
-
+ 
 variable "env" {
-  default = "dev"
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
 }
